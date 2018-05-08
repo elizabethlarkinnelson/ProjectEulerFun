228870e1c1b6b659defa7c77efb672b2ca3182ad
Elizabeth Nelson <ENelson@travisusd.org>
2018-05-08 10:18:57 -0700

    largest prime complete

--- PE_one.py
+++ PE_one.py
@@ -31,4 +31,34 @@ def fibo_even(num):
 		total = first + second
 		first = second
 		second = total
-	
+
+#3. Largest Prime Factor
+
+import math
+
+def largest_prime(n):
+
+	# The below code works, it is not time efficient
+	# prime = 1
+
+	# for n in range(2, num + 1):
+	# 	for i in range (2, n):
+	# 		if n % i == 0:
+	# 			break
+	# 		else:
+	# 			if i > prime:
+	# 				prime = n
+	# return prime
+
+	#NEED TO COME BACK TO THISE ONE***************************
+	return
+
+
+#4. Largest Palindrome Product
+
+def largest_palindrome():
+	'''
+	This function returns the largest possible palindrome number from two, three digit products
+	'''
+
+	return
